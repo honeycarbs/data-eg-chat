2a0d69616213431ca8dd6619e3dda8bc87bfcf4d
hhk63 <hhk2@pdx.edu>
2025-04-07 22:30:18 -0700

    implemented fetcher

---
+++ Jupiter/fetch.py
@@ -0,0 +1,16 @@
+# Python program that can make an HTTP request to a web server
+# and fetch the results.
+
+import urllib.request
+import urllib.parse
+
+def fetchData(vehicleID, oFile):
+    url = f"https://busdata.cs.pdx.edu/api/getBreadCrumbs?vehicle_id={urllib.parse.quote(vehicleID)}"
+
+    response = urllib.request.urlopen(url)
+    data = response.read()
+
+    with open(oFile, 'wb') as file:
+        file.write(data)
+
+
