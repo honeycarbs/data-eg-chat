7927be11e2451904451819ee8f38d9cdbd807f8a
Akira <smedley@pdx.edu>
2025-04-09 11:52:29 -0700

    Modified main to run fetch, updated fetch to have error handling

---
+++ Jupiter/main.py
@@ -0,0 +1,22 @@
+from fetch import fetchData
+from datetime import date
+
+def text_file_to_list(file_path):
+    try:
+        with open(file_path, 'r') as file:
+            lines = [line.strip() for line in file]
+        return lines
+    except FileNotFoundError:
+        return f"Error: File not found at '{file_path}'"
+    except Exception as e:
+        return f"An error occurred: {e}"
+
+def main():
+    today = date.today()
+    today = today.strftime("%Y-%m-%d")
+    list = text_file_to_list('id.txt')
+    for id in list:
+        fetchData(id, id + "-" + today)
+
+if __name__ == "__main__":
+    main()
