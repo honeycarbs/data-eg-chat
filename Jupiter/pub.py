27e8c0bf9f2e10a11633aa9b1bded059ad696333
Akira <smedley@pdx.edu>
2025-04-16 00:33:59 -0700

    Removed print statements

--- Jupiter/pub.py
+++ Jupiter/pub.py
@@ -6,11 +6,6 @@ def publish(msg):
     topic_id = "Breadcrumb_Storage"
     publisher = pubsub_v1.PublisherClient()
     topic_path = publisher.topic_path(project_id, topic_id)
-
-    
     data_str = msg
     data = data_str.encode("utf-8")
     future = publisher.publish(topic_path, data)
-    print(future.result())
-
-    print(f"Published messages to {topic_path}.")
