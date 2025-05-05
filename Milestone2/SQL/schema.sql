82d17b88889d071cfaec5f1ae5936766e6cfc75f
honeycarbs <honeycarbs.sub@gmail.com>
2025-05-05 12:27:43 -0700

    Transfer SQL syntax to caps

--- Milestone2/SQL/schema.sql
+++ Milestone2/SQL/schema.sql
@@ -3,8 +3,8 @@ DROP TABLE IF EXISTS Trip;
 DROP TYPE IF EXISTS service_type;
 DROP TYPE IF EXISTS tripdir_type;
 
-CREATE TYPE service_type as enum ('Weekday', 'Saturday', 'Sunday');
-CREATE TYPE tripdir_type as enum ('Out', 'Back');
+CREATE TYPE service_type AS enum ('Weekday', 'Saturday', 'Sunday');
+CREATE TYPE tripdir_type AS enum ('Out', 'Back');
 
 CREATE TABLE Trip (
         trip_id integer,
